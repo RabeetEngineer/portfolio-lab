324d43afee919c02afd66f423f703216867eb2dc
Hafiz Rabeet Ahmad <muhammadrabeet@gmail.com>
2023-10-12 17:33:10 +0500

    second commit

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
-const bio1 = "A frontend developer";
+const greeting = "Hello, I am Hafiz Rabeet Ahmad!";
+const bio1 = "A Frontend Developer";
 const bio2 = "specialised in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
@@ -15,6 +15,21 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+
+  <VStack spacing={16}>
+    <Avatar src={"https://i.pravatar.cc/150?img=7"}
+    size='2xl'
+    name='Hafiz Rabeet Ahmad'
+    />
+    <Heading as='h4' size='md' noOfLines={1}>
+      {greeting}
+    </Heading>
+
+    <VStack spacing={16}>
+      <Heading as='h1' size='3xl' noOfLines={1}>{bio1}</Heading>
+    </VStack>
+
+  </VStack>
 
   </FullScreenSection>
 );
